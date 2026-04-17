03bd81dbb232291d7b86f7778f7f8dd32ca242d0
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-04-17 08:03:57 +0300

    Added seed data for the exercises

--- server/seed.py
+++ server/seed.py
@@ -9,3 +9,12 @@ with app.app_context():
     Exercise.query.delete()
     db.session.commit()
     
+    e1 = Exercise(name='Push-Up', category='Strength', equipment_needed=False)
+    e2 = Exercise(name='Running', category='Cardio', equipment_needed=True)
+    e3 = Exercise(name='Yoga', category='Flexibility', equipment_needed=False)
+    e4 = Exercise(name='Plank', category='Balance', equipment_needed=False)
+    e5 = Exercise(name='Jumping Jacks', category='Cardio', equipment_needed=False)
+    e6 = Exercise(name='Dumbell Curl', category='Strength', equipment_needed=True)
+    
+    db.session.add_all([e1, e2, e3, e4, e5, e6])
+    db.session.commit()
